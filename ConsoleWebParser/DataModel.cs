abab56e10e5d168cf63698bc8717d2e6a168b271
meggs85 <59177704+meggs85@users.noreply.github.com>
2020-08-14 21:49:44 +0300

    Add files via upload

---
+++ ConsoleWebParser/DataModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebParser
+{
+    public class DataException : Exception
+    {
+        public DataException(string msg) : base(msg) { }
+    }
+
+    public class DataModel
+    {
+        public const string InvalidValue = "N/A";
+        public int id = -1;
+        public string name = InvalidValue;
+        public string VIN = InvalidValue;
+        public double price = 0.0;
+        public string URL = InvalidValue;
+
+        public DataModel()
+        {
+            id = GetHashCode(); // ...
+        }
+
+        public DataModel(string name_, string vin_, string url_, double price_) : base()
+        {
+            name = name_;
+            VIN = vin_;
+            URL = url_;
+            price = price_;
+        }
+
+        public void Validate()
+        {
+            if (name == InvalidValue || VIN == InvalidValue || URL == InvalidValue || price <= 0) throw new DataException("parce results invalid");
+        }
+        
+        public void Output()
+        {
+            Console.WriteLine($"Entry {id} ({name}): vin = {VIN}, url = {URL}, price = {price}");
+        }
+    }
+}
